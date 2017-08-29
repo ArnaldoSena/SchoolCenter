077baebffbd35d3f071283625773233bbbabc7cc
Arnaldo Sena <arnaldoufcg@gmail.com>
2017-08-29 10:47:04 -0300

    jsp falhando

---
+++ schoocenter/schoolDB/src/main/resources/br/com/artusena/Pessoa.java
@@ -0,0 +1,21 @@
+package br.com.artusena;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Pessoa
+ *
+ */
+@Entity
+
+public class Pessoa implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Pessoa() {
+		super();
+	}
+   
+}
